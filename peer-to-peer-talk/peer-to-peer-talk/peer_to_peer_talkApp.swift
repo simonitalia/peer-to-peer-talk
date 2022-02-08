026bcb8e23e93a9b1b0c94a61bd08c536e97bf48
Simon Italia <simonitaliaatwork@gmail.com>
2022-02-08 15:13:02 +0100

    initial project commit

---
+++ peer-to-peer-talk/peer-to-peer-talk/peer_to_peer_talkApp.swift
@@ -0,0 +1,17 @@
+//
+//  peer_to_peer_talkApp.swift
+//  peer-to-peer-talk
+//
+//  Created by Simon Italia on 08/02/22.
+//
+
+import SwiftUI
+
+@main
+struct peer_to_peer_talkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
