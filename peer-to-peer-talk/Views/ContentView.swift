4d0e1ea5fd09e68b78e296d11241cd601ae09d01
Gleswam <gleswam@gmail.com>
2022-02-17 22:02:07 +0100

    added app icon (test)

---
+++ peer-to-peer-talk/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  peer-to-peer-talk
+//
+//  Created by Simon Italia on 08/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
