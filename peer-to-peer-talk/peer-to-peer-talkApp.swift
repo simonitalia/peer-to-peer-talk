1cd32dd8954ce9e6e944d004f2bbff5c6cd4a705
Simon Italia <simonitaliaatwork@gmail.com>
2022-02-18 16:39:08 +0100

    git merge: resolve conflicts

--- peer-to-peer-talk/peer-to-peer-talkApp.swift
+++ peer-to-peer-talk/peer-to-peer-talkApp.swift
@@ -12,12 +12,8 @@ struct peer_to_peer_talkApp: App {
     @StateObject var chatHelper = ChatHelper()
     var body: some Scene {
         WindowGroup {
-<<<<<<< HEAD:peer-to-peer-talk/peer-to-peer-talkApp.swift
             ChatViewScreen().environmentObject(chatHelper)
-=======
-//            ContentView()
             OnboardingFirst()
->>>>>>> c1f7ec1 (onboarding screens):peer-to-peer-talk/PeerToPeerTalkApp.swift
         }
     }
 }
