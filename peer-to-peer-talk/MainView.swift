b9e85ce4c845e2ea6562b3d49ca724320f9a36d7
Simon Italia <sitalia@magicaltomato.com>
2022-02-20 01:51:07 +0100

    feature: tab view
    - set default / launch tab view item

--- peer-to-peer-talk/MainView.swift
+++ peer-to-peer-talk/MainView.swift
@@ -8,8 +8,8 @@
 import SwiftUI
 
 struct MainView: View {
-	@State var selectedTabViewItem = 1
-	
+	@State var selectedTabViewItem = 0
+
     var body: some View {
 		
 		TabView(selection: $selectedTabViewItem) {
