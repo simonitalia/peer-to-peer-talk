178cdb365295fbb2176e4c45ba2041e3e21ed5b5
Simon Italia <sitalia@magicaltomato.com>
2022-02-20 01:51:46 +0100

    feature: chat view screen
    - wrap view in nav view

--- peer-to-peer-talk/ChatViewScreen.swift
+++ peer-to-peer-talk/ChatViewScreen.swift
@@ -9,58 +9,49 @@ import SwiftUI
 
 struct ChatViewScreen: View {
     @State private var typingMessage = ""
-    @EnvironmentObject var chatHelper: ChatHelper
+    @State var chatHelper = ChatHelper()
     @ObservedObject private var keyboard = KeyboardResponder()
 
-    
-    init() {
-        UITableView.appearance().separatorStyle = .none
-        UITableView.appearance().tableFooterView = UIView()
-    }
-    
-    
     var body: some View {
-        ScrollViewReader { proxy in
-            VStack {
-                List {
-                    ForEach(chatHelper.realTimeMessages) { message in
-                        TextBubbleView(message: message)
-                    }
-                }
-                
-                
-                
-                Spacer()
-                HStack {
-                    TextField("Type something", text: $typingMessage)
-                        .font(.system(size: 15, weight: .regular))
-                        .padding(.all, 15)
-                        .textFieldStyle(.roundedBorder)
-                        .onSubmit {
-                            sendMessage()
-                        }
-                    Button {
-                        sendMessage()
-                        
-                    } label: {
-                        Image(systemName: "arrow.up.circle")
-                            .font(.system(size: 25, weight: .regular))
-                            .foregroundColor(Color.primary)
-                    }
-                }
-                .padding(.all, 15)
-            }
-            
-            .onAppear {
-                proxy.scrollTo(chatHelper.realTimeMessages.last)
-            }
-//            .offset(y: -keyboard.currentHeight * 0.9 )
-            .edgesIgnoringSafeArea(keyboard.currentHeight == 0.0 ? .leading: .bottom)
-            
-            
-        }
+		
+		NavigationView {
+			ScrollViewReader { proxy in
+				VStack {
+					List {
+						ForEach(chatHelper.realTimeMessages) { message in
+							TextBubbleView(message: message)
+						}
+					}
+					
+					Spacer()
+					
+					HStack {
+						TextField("Type something", text: $typingMessage)
+							.font(.system(size: 15, weight: .regular))
+							.padding(.all, 15)
+							.textFieldStyle(.roundedBorder)
+							.onSubmit {
+								sendMessage()
+							}
+						
+						Button {
+							sendMessage()
+						} label: {
+							Image(systemName: "arrow.up.circle")
+								.font(.system(size: 25, weight: .regular))
+								.foregroundColor(Color.primary)
+						}
+					}
+					.padding(.all, 15)
+				}
+				.onAppear {
+					proxy.scrollTo(chatHelper.realTimeMessages.last)
+				}
+				
+			}
+			.edgesIgnoringSafeArea(keyboard.currentHeight == 0.0 ? .leading : .bottom)
+		}
     }
-    
     
     func sendMessage() {
         let newMessage = Message(content: typingMessage, user: DataSource.secondUser, isReceived: false)
@@ -68,7 +59,6 @@ struct ChatViewScreen: View {
         typingMessage = ""
     }
 }
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
