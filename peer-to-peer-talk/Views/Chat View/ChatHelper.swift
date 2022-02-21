f427c7e8c4418794c9cf2ebda97f5cec2167f89f
Simon Italia <sitalia@magicaltomato.com>
2022-02-21 03:39:50 +0100

    refactor: chat helper
    - migrated test data source from user model
    
    - renamed fake data source

--- peer-to-peer-talk/Views/Chat View/ChatHelper.swift
+++ peer-to-peer-talk/Views/Chat View/ChatHelper.swift
@@ -8,11 +8,29 @@
 import Combine
 
 class ChatHelper: ObservableObject {
-    var didChange = PassthroughSubject<Void, Never>()
-    @Published var realTimeMessages = DataSource.messages
-    
-    func sendMessage(_ chatMessage: Message) {
-        realTimeMessages.append(chatMessage)
-        didChange.send(())
-    }
+	var didChange = PassthroughSubject<Void, Never>()
+	@Published var realTimeMessages = FakeDataSource.messages
+
+	func sendMessage(_ chatMessage: Message) {
+		realTimeMessages.append(chatMessage)
+		didChange.send(())
+	}
+}
+
+struct FakeDataSource {
+	static let firstUser = User()
+	static var secondUser = User(isCurrentUser: true)
+	static let messages = [
+		Message(text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", user: FakeDataSource.firstUser),
+		Message(text: "Lacus luctus accumsan tortor posuere ac ut consequat.", user: FakeDataSource.secondUser),
+		Message(text: "😇", user: FakeDataSource.firstUser, isReceived: true),
+		Message(text: "Vitae tortor condimentum lacinia quis vel eros donec ac.", user: FakeDataSource.firstUser),
+		Message(text: "Aliquam sem et tortor consequat id porta nibh venenatis.", user: FakeDataSource.secondUser),
+		Message(text: "Ante in nibh mauris cursus mattis molestie a.", user: FakeDataSource.firstUser),
+		
+		Message(text: "😇", user: FakeDataSource.firstUser, isReceived: true),
+		Message(text: "Vitae tortor condimentum lacinia quis vel eros donec ac.", user: FakeDataSource.firstUser),
+		Message(text: "Aliquam sem et tortor consequat id porta nibh venenatis.", user: FakeDataSource.secondUser),
+		Message(text: "Ante in nibh mauris cursus mattis molestie a.", user: FakeDataSource.firstUser)
+	]
 }
