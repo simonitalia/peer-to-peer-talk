9ad46101f287231cf5b4231d5aa7b0d3228f56bc
Simon Italia <sitalia@magicaltomato.com>
2022-02-21 04:47:33 +0100

    refactor:  chat helper
    - modify test / fake data

--- peer-to-peer-talk/Views/Chat View/ChatHelper.swift
+++ peer-to-peer-talk/Views/Chat View/ChatHelper.swift
@@ -5,32 +5,83 @@
 //  Created by Анастасия Скорюкова on 17/02/22.
 //
 
+import Foundation
 import Combine
 
 class ChatHelper: ObservableObject {
 	var didChange = PassthroughSubject<Void, Never>()
 	@Published var realTimeMessages = FakeDataSource.messages
 
-	func sendMessage(_ chatMessage: Message) {
+	func sendMessage(_ chatMessage: FakeMessage) {
 		realTimeMessages.append(chatMessage)
 		didChange.send(())
 	}
 }
 
 struct FakeDataSource {
-	static let firstUser = User()
-	static var secondUser = User(isCurrentUser: true)
+	static let firstUser = FakeUser(name: "Jack", isCurrentUser: false)
+	static var secondUser = FakeUser(name: "Jill", isCurrentUser: true)
 	static let messages = [
-		Message(text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", user: FakeDataSource.firstUser),
-		Message(text: "Lacus luctus accumsan tortor posuere ac ut consequat.", user: FakeDataSource.secondUser),
-		Message(text: "😇", user: FakeDataSource.firstUser, isReceived: true),
-		Message(text: "Vitae tortor condimentum lacinia quis vel eros donec ac.", user: FakeDataSource.firstUser),
-		Message(text: "Aliquam sem et tortor consequat id porta nibh venenatis.", user: FakeDataSource.secondUser),
-		Message(text: "Ante in nibh mauris cursus mattis molestie a.", user: FakeDataSource.firstUser),
+		FakeMessage(
+		content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+			user: FakeDataSource.firstUser,
+			isReceived: false),
 		
-		Message(text: "😇", user: FakeDataSource.firstUser, isReceived: true),
-		Message(text: "Vitae tortor condimentum lacinia quis vel eros donec ac.", user: FakeDataSource.firstUser),
-		Message(text: "Aliquam sem et tortor consequat id porta nibh venenatis.", user: FakeDataSource.secondUser),
-		Message(text: "Ante in nibh mauris cursus mattis molestie a.", user: FakeDataSource.firstUser)
+		FakeMessage(
+			content: "Lacus luctus accumsan tortor posuere ac ut consequat.",
+			user: FakeDataSource.secondUser,
+			isReceived: false),
+		
+		FakeMessage(
+			content: "😇",
+			user: FakeDataSource.firstUser,
+			isReceived: true),
+		
+		FakeMessage(
+			content: "Vitae tortor condimentum lacinia quis vel eros donec ac.",
+			user: FakeDataSource.firstUser,
+			isReceived: true),
+		
+		FakeMessage(
+			content: "Aliquam sem et tortor consequat id porta nibh venenatis.",
+			user: FakeDataSource.secondUser,
+			isReceived: false),
+		
+		FakeMessage(
+			content: "Ante in nibh mauris cursus mattis molestie a.",
+				user: FakeDataSource.firstUser,
+			isReceived: true),
+		
+		FakeMessage(
+			content: "😇",
+			user: FakeDataSource.firstUser,
+			isReceived: true),
+		
+		FakeMessage(
+			content: "Vitae tortor condimentum lacinia quis vel eros donec ac.",
+			user: FakeDataSource.firstUser,
+			isReceived: true),
+		
+		FakeMessage(
+			content: "Aliquam sem et tortor consequat id porta nibh venenatis.",
+			user: FakeDataSource.secondUser,
+			isReceived: false),
+		
+		FakeMessage(
+			content: "Ante in nibh mauris cursus mattis molestie a.",
+			user: FakeDataSource.firstUser,
+			isReceived: true)
 	]
+}
+
+struct FakeUser {
+	let name: String
+	let isCurrentUser: Bool
+}
+
+struct FakeMessage: Identifiable {
+	var id = UUID()
+	let content: String
+	let user: FakeUser
+	let isReceived: Bool
 }
