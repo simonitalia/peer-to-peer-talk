45105b40adcdf3de76af3953cca66f5c2c578806
Simon Italia <sitalia@magicaltomato.com>
2022-02-21 03:43:27 +0100

    refactor: text bubble view
    - update references to renamed properties

--- peer-to-peer-talk/Views/Chat View/TextBubbleView.swift
+++ peer-to-peer-talk/Views/Chat View/TextBubbleView.swift
@@ -17,7 +17,7 @@ struct TextBubbleView: View {
                 .font(.system(size: 15, weight: .semibold))
                 .foregroundColor(.white)
                 .padding(.all)
-                .background(message.user.isCurrentUser ? Color("currentUserBubbleColor") : Color("BubbleColor"))
+                .background(message.author.isCurrentUser ? Color("currentUserBubbleColor") : Color("BubbleColor"))
                 .clipShape(RoundedRectangle(cornerRadius: 15))
         }
         .frame(maxWidth: .infinity, alignment: message.isReceived ? .leading : .trailing)
@@ -29,7 +29,7 @@ struct TextBubbleView: View {
 
 struct TextBubbleView_Previews: PreviewProvider {
     static var previews: some View {
-        TextBubbleView(message: DataSource.messages[0])
+        TextBubbleView(message: FakeDataSource.messages[0])
     }
 }
 
