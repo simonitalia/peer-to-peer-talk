2ce7dc5d2bb3da7a29b8f987a7e0a0dc19e89bad
Simon Italia <sitalia@magicaltomato.com>
2022-02-21 03:31:17 +0100

    refactor: message model
    - implement initializer
    
    - rename properties

--- peer-to-peer-talk/Models/Message.swift
+++ peer-to-peer-talk/Models/Message.swift
@@ -7,9 +7,15 @@
 
 import Foundation
 
-struct Message: Hashable, Identifiable {
-	let id = UUID()
-    var content: String
-    let user: User
-    var isReceived: Bool
+struct Message: Hashable, Identifiable, Codable {
+	var id = UUID()
+    let content: String
+    let author: User
+    var isReceived: Bool //for testing with fake data source
+	
+	init(text content: String, user author: User, isReceived: Bool = false) {
+		self.content = content
+		self.author = author
+		self.isReceived = isReceived
+	}
 }
