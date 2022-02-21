639eb010331a8fc8b4f40bb15a74f062258c6476
Simon Italia <sitalia@magicaltomato.com>
2022-02-21 04:45:33 +0100

    feature: chat view screen
    - hook up chat view screen with multiplier framework logic
    
    - update view when message is send and received
    
    - comment logic related to test / fake data

--- peer-to-peer-talk/Views/Chat View/ChatView.swift
+++ peer-to-peer-talk/Views/Chat View/ChatView.swift
@@ -8,9 +8,15 @@
 import SwiftUI
 
 struct ChatView: View {
-    @State private var typingMessage = ""
-    @State var chatHelper = ChatHelper()
+	@EnvironmentObject var user: User
+	@EnvironmentObject var mcServiceManager: MCServiceManager
+	
     @ObservedObject private var keyboard = KeyboardResponder()
+	
+	@State private var chatThread = [Message]()
+	@State private var typedText = ""
+	
+	@State var chatHelper = ChatHelper() // for testing with fake data
 
     var body: some View {
 		
@@ -18,24 +24,25 @@ struct ChatView: View {
 			ScrollViewReader { proxy in
 				VStack {
 					List {
-						ForEach(chatHelper.realTimeMessages) { message in
-							TextBubbleView(message: message)
+//						ForEach(chatHelper.realTimeMessages) { message in
+						ForEach(chatThread) { message in
+							TextBubbleView(user: user, message: message)
 						}
 					}
 					
 					Spacer()
 					
 					HStack {
-						TextField("Type something", text: $typingMessage)
+						TextField("Type something", text: $typedText)
 							.font(.system(size: 15, weight: .regular))
 							.padding(.all, 15)
 							.textFieldStyle(.roundedBorder)
 							.onSubmit {
-								sendMessage()
+								self.sendMessage()
 							}
 						
 						Button {
-							sendMessage()
+							self.sendMessage()
 						} label: {
 							Image(systemName: "arrow.up.circle")
 								.font(.system(size: 25, weight: .regular))
@@ -44,8 +51,21 @@ struct ChatView: View {
 					}
 					.padding(.all, 15)
 				}
+				
 				.onAppear {
-					proxy.scrollTo(chatHelper.realTimeMessages.last)
+//					proxy.scrollTo(chatHelper.realTimeMessages.last)
+					proxy.scrollTo(chatThread.last)
+				}
+				
+				// upddate local data source with received message
+				.onChange(of: mcServiceManager.receivedMessages) { _ in
+					if let message = mcServiceManager.receivedMessages.last {
+						self.chatThread.append(message)
+					}
+				}
+				
+				.onChange(of: chatThread) { _ in
+					proxy.scrollTo(chatThread.last)
 				}
 				
 			}
@@ -53,15 +73,30 @@ struct ChatView: View {
 		}
     }
     
-    func sendMessage() {
-        let newMessage = Message(content: typingMessage, user: DataSource.secondUser, isReceived: false)
-        chatHelper.sendMessage(newMessage)
-        typingMessage = ""
+	func sendMessage() {
+		guard !typedText.isEmpty else { return }
+		
+//		let newMessage = Message(content: typingMessage, user: DataSource.secondUser, isReceived: false)
+//		chatHelper.sendMessage(newMessage)
+		
+		let newMessage = Message(
+			text: typedText,
+			user: user
+		)
+
+		//update local data source with typed message and send message to peers
+		self.chatThread.append(newMessage)
+		self.mcServiceManager.send(message: newMessage)
+
+		//reset text field
+		self.typedText.removeAll()
     }
 }
 
 struct ChatView_Previews: PreviewProvider {
     static var previews: some View {
         ChatView()
+			.environmentObject(User.getUser())
+			.environmentObject(MCServiceManager(user: User.getUser()))
     }
 }
