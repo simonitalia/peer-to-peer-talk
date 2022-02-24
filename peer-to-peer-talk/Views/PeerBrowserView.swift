b471cf8a41c310fc64d909bee5c7d8f5fd004a43
Simon Italia <simonitaliaatwork@gmail.com>
2022-02-24 20:44:04 +0100

    feature: peer browser view
    - dismiss peer browser on press of done or cancel buttons
    
    - prevent swipe to close
    
    - implement navigation tool bar action buttons
    
    - implement end session when chat ends
    
    - hook up view with view browser coordinator

--- peer-to-peer-talk/Views/PeerBrowserView.swift
+++ peer-to-peer-talk/Views/PeerBrowserView.swift
@@ -10,15 +10,39 @@ import SwiftUI
 struct PeerBrowserView: View {
 	@EnvironmentObject var user: User
 	@EnvironmentObject var mcServiceManager: MCServiceManager
+    
+    @State private var isPresententingMCBrowserViewController: Bool = false
 	
-	@State var presentMCBrowserViewController = false
-	
-    var body: some View {
-
-		ZStack {
-			mcServiceManager.presentMCPeerBrowserViewController(serviceManager: mcServiceManager)
-		}
-	}
+        var body: some View {
+            NavigationView {
+                ZStack {
+                    Text("PeerBrowser")
+                }
+                
+                // Navigation
+                .navigationTitle("Display Name: \(user.name)")
+                .toolbar {
+                    Button("Start New Chat") {
+                        isPresententingMCBrowserViewController.toggle()
+                        
+                        //disconnet from current session
+                        mcServiceManager.session.disconnect()
+                    }
+                }
+            }
+            .onAppear {
+                if mcServiceManager.session.connectedPeers.isEmpty {
+                    isPresententingMCBrowserViewController.toggle()
+            }
+        }
+        .sheet(isPresented: $isPresententingMCBrowserViewController) {
+            MCPeerBrowserViewController(
+                         serviceManager: mcServiceManager,
+                         isPresented: $isPresententingMCBrowserViewController)
+                .interactiveDismissDisabled() //prevent swipe to dismiss
+        }
+        
+    }
 }
 
 struct PeerBrowserView_Previews: PreviewProvider {
